306c8ad32103c531e33ded4274b170bd99aa5f2c
Lee Robbie <leerobb198@lees-mbp.home>
2020-02-12 19:59:24 +0000

    .

--- runningPython.php
+++ runningPython.php
@@ -5,7 +5,7 @@
 <h1>Here be pythons</h1>
 
 <?php
-$output = shell_exec('python r2.py -r 6 -n 1 -x 49');
+$output = shell_exec('python r1.py');
 echo "<p> The python script said </p>";
 echo "<pre>$output</pre>";
 ?>
